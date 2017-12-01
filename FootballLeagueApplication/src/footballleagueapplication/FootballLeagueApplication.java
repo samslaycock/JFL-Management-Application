82ec51d840bfcdb1fefcec7e61e42c0c8b0769fa
samslaycock <samslaycock@gmail.com>
2017-12-01 23:23:52 +0000

    Revert "Removed duplicate class."
    This reverts commit 32f58efdb6a1c103a52b84918c9e4dc5f41712d9.

---
+++ FootballLeagueApplication/src/footballleagueapplication/FootballLeagueApplication.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package footballleagueapplication;
+
+import GUI.MenuForm;
+
+/**
+ *
+ * @author blao
+ */
+public class FootballLeagueApplication {
+
+    /*
+     * Launches the menu for the JFL management progrram
+     * 
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MenuForm menu = new MenuForm();
+        menu.setVisible(true);
+    }
+    
+}
